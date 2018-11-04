4fa5000b72e426094739ecf5e4c95abfbae52a0c
Ahsan Barkati <ahsanbarkati@gmail.com>
2018-11-05 02:25:42 +0530

    Fix breaks while draw

--- paint.py
+++ paint.py
@@ -1,39 +1,29 @@
-import pygame
-from pygame.locals import *
-import sys, os
-import time
+from tkinter import *
 
-pygame.init()
-pygame.display.set_caption('Paintme')
-mouse = pygame.mouse
-fpsClock = pygame.time.Clock()
+canvas_width = 500
+canvas_height = 150
+xp=0
+yp=0
 
-width = 720
-height = 440
+def naya (event):
+	global xp 
+	global yp
+	xp, yp = ( event.x  ), ( event.y  )
+def paint( event ):
+   global xp 
+   global yp
+   x, y = ( event.x  ), ( event.y  )
+   print(x,y)
+   w.create_line( xp, yp, x, y ,width=5)
+   xp, yp = ( event.x  ), ( event.y  )
 
-window = pygame.display.set_mode((width, height))
-canvas = window.copy()
+master = Tk()
+w = Canvas(master, width=canvas_width, height=canvas_height)
+w.pack(expand = YES, fill = BOTH)
+w.bind( "<B1-Motion>", paint )
+w.bind( "<Button-1> ", naya)
 
-BLACK = pygame.Color( 0 ,  0 ,  0 )
-WHITE = pygame.Color(255, 255, 255)
-
-left_pressed, middle_pressed, right_pressed = mouse.get_pressed()
-canvas.fill(WHITE)
-prevcod=pygame.mouse.get_pos()
-while True:
-  for event in pygame.event.get():
-    left_pressed, middle_pressed, right_pressed = mouse.get_pressed()
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
-    elif left_pressed:
-      cod=pygame.mouse.get_pos()
-      #pygame.draw.circle(canvas, BLACK, (cod),5)
-      print(cod)
-      pygame.draw.line(canvas, BLACK, prevcod, cod, 5) 
-      prevcod=cod
-  window.fill(WHITE)
-  window.blit(canvas, (0, 0))
-  time.sleep(0.05)
-  #pygame.draw.circle(window, BLACK, (pygame.mouse.get_pos()), 5)
-  pygame.display.update()
+message = Label( master, text = "Press and Drag the mouse to draw" )
+message.pack( side = BOTTOM )
+	
+mainloop()
