2e6eecd7b2cab9c7b70d3a7e49e78eb75e991e01
shivank <shivankgarg98@gmail.com>
2018-11-08 01:03:36 +0530

    add eraser and brush size

--- paint.py
+++ paint.py
@@ -1,10 +1,36 @@
 from tkinter import *
+
 import socket 
 import select 
 import sys 
 import json
-  
+from tkinter.colorchooser import *
+
+color=(1,'red')
+def getColor():
+    global color
+    color = askcolor()
+def eraser():
+    global color
+    color = (2,'#d9d9d9')
+
+def naya(event):
+	global xp 
+	global yp
+	xp, yp = ( event.x  ), ( event.y  )
+def paint(event):
+   global xp 
+   global yp
+   x, y = ( event.x  ), ( event.y  )
+   #print(x,y)
+   w.create_line( xp, yp, x, y ,width=float(scale.get()), fill=color[1])
+   message=str(xp)+'|'+str(yp)+'|'+str(x)+'|'+str(y)
+   server.send(bytes(message,'utf8'))
+   xp, yp = ( event.x  ), ( event.y  )
+
+#Button(text='Select Color', command=getColor).pack()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
 if len(sys.argv) != 3: 
     print("Correct usage: script, IP address, port number")
     exit() 
@@ -21,27 +47,24 @@ canvas_height = 150
 xp=0
 yp=0
 
-def naya (event):
-	global xp 
-	global yp
-	xp, yp = ( event.x  ), ( event.y  )
-def paint( event ):
-   global xp 
-   global yp
-   x, y = ( event.x  ), ( event.y  )
-   #print(x,y)
-   w.create_line( xp, yp, x, y ,width=5)
-   message=str(xp)+'|'+str(yp)+'|'+str(x)+'|'+str(y)
-   server.send(bytes(message,'utf8'))
-   xp, yp = ( event.x  ), ( event.y  )
-
 master = Tk()
 w = Canvas(master, width=canvas_width, height=canvas_height)
 w.pack(expand = YES, fill = BOTH)
 w.bind( "<B1-Motion>", paint )
 w.bind( "<Button-1> ", naya)
 
-message = Label( master, text = "You know what to do now. :-P" )
+button1 = Button(text = "Color", command = getColor, anchor = W)
+button1.configure(width = 10, activebackground = "#33B5E5", relief = RAISED,bg=color[1])
+button1_window = w.create_window(10, 10, anchor=NW, window=button1)
+
+scale = Scale(master, from_=1, to=15, orient=HORIZONTAL)
+scale.pack()
+eraser = Button(text = 'eraser', command = eraser, anchor = W)
+eraser.configure(width = 10, activebackground = "#33B5E5", relief = RAISED, bg='#653656')
+eraser_window = w.create_window(10,60, anchor=NW, window=eraser)
+
+
+message = Label( master, text = "You know what to do now" )
 message.pack( side = BOTTOM )
 while 1:
    for socks in read_sockets: 
@@ -59,7 +82,7 @@ while 1:
                if(len(cords)):
                   w.create_line( cords[0], cords[1], cords[2], cords[3] ,width=5)
             except socket.timeout:
-               print('')    
+             '''  print('')'''    
    master.update_idletasks()
    master.update()
 #mainloop()
