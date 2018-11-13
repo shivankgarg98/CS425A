7754a86b3e560f3edae9a08d645c0b93e67cb34a
bnithishr <bnithish@iitk.ac.in>
2018-11-13 20:12:24 +0530

    resolution thing done

--- paint.py
+++ paint.py
@@ -19,18 +19,24 @@ def eraser():
     w.configure(cursor="pirate") #change cursor to indicate deletion
 
 def naya(event):
-	global xp 
-	global yp
-	xp, yp = ( event.x ), ( event.y )
+    global xp 
+    global yp
+    global wid
+    global hi
+    xp, yp = ( event.x ), ( event.y )
+    wid=w.winfo_width()
+    hi=w.winfo_height()
 
 def paint(event):
-   global xp 
-   global yp
-   x, y = ( event.x  ), ( event.y  )
-   w.create_line( xp, yp, x, y ,width=float(scale.get()), fill=color[1])
-   message=str(xp)+'|'+str(yp)+'|'+str(x)+'|'+str(y)
-   server.send(bytes(message,'utf8'))
-   xp, yp = ( event.x  ), ( event.y  )
+    global xp 
+    global yp
+    global wid
+    global hi
+    x, y = ( event.x  ), ( event.y  )
+    w.create_line( xp, yp, x, y ,width=float(scale.get()), fill=color[1])
+    message=str(xp)+'|'+str(yp)+'|'+str(x)+'|'+str(y)+'|'+str(hi)+'|'+str(wid)
+    server.send(bytes(message,'utf8'))
+    xp, yp = ( event.x  ), ( event.y  )
 
 #Button(text='Select Color', command=getColor).pack()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -50,6 +56,8 @@ canvas_width = 500
 canvas_height = 150
 xp=0
 yp=0
+wid=canvas_width
+hi=canvas_height
 
 master = Tk()
 w = Canvas(master, width=canvas_width, height=canvas_height)
@@ -72,23 +80,23 @@ eraser_window = w.create_window(10,50, anchor=NW, window=eraser)
 message = Label( master, text = "You know what to do now" )
 message.pack( side = BOTTOM )
 while 1:
-   for socks in read_sockets: 
+    for socks in read_sockets: 
         if socks == server:
             try: 
-               mes = socks.recv(2048)
-               mes=mes.decode()
-               cords=[]
-               if(mes[0]=='<'):
-                  mes=mes.split('>')[1]
-                  mes=mes.split('|')
-                  for i in mes:
-                     cords.append(int(i))
-               print(cords)
-               if(len(cords)):
-                  w.create_line( cords[0], cords[1], cords[2], cords[3] ,width=5)
+                mes = socks.recv(2048)
+                mes=mes.decode()
+                cords=[]
+                if(mes[0]=='<'):
+                    mes=mes.split('>')[1]
+                    mes=mes.split('|')
+                    for i in mes:
+                       cords.append(int(i))
+                print(cords)
+                if(len(cords)):
+                    w.create_line( cords[0]*wid/cords[4], cords[1]*hi/cords[5], cords[2]*wid/cords[4], cords[3]*hi/cords[5] ,width=float(scale.get()), fill=color[1])
             except socket.timeout:
-             '''  print('')'''    
-   master.update_idletasks()
-   master.update()
+                '''  print('')'''    
+    master.update_idletasks()
+    master.update()
 #mainloop()
 server.close()
