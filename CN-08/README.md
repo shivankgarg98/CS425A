778b75a57f20657b8fbef30c53872c440277f3b0
shivank <shivankgarg98@gmail.com>
2018-11-15 20:21:33 +0530

    final

---
+++ CN-08/README.md
@@ -0,0 +1,8 @@
+#paint
+## Dependencies
+
+## Running the application
+1. Run the server by `python3 server.py IP-ADDR PORT`
+2. Run the voice server by `python3 voice_ser.py IP-ADDR PORT1`
+3. Run the paint application by `python3 paint.py IP-ADDR PORT`
+4. Run the Voice application by `python3 voice_cli.py IP-ADDR PORT1`
---
+++ CN-08/voice_cli.py
@@ -0,0 +1,53 @@
+import socket
+import sys
+import time
+import select
+import pyaudio
+from _thread import *
+
+CHUNK=1024
+FORMAT=pyaudio.paInt16
+CHANNELS=1
+RATE=20000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+if len(sys.argv) != 3: 
+    print("Correct usage: script, IP address, port number")
+    exit() 
+IP_address = str(sys.argv[1]) 
+Port = int(sys.argv[2]) 
+server.connect((IP_address, Port)) 
+sockets_list = [sys.stdin, server] 
+
+read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
+server.settimeout(0.035)
+
+p=pyaudio.PyAudio()
+recv_stream=p.open(format=FORMAT,channels=CHANNELS,rate=RATE,output=True,frames_per_buffer=CHUNK)
+send_stream=p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK)
+
+def recv_data():
+    while True:
+        try:
+            for socks in read_sockets:
+                if socks==server:
+                    data=socks.recv(1024)
+                    recv_stream.write(data)
+                else:
+                    pass
+        except:
+            continue
+
+def send_data():
+    while True:
+        try:
+            data = send_stream.read(CHUNK)
+            server.send(data)
+        except:
+            pass
+
+start_new_thread(recv_data, ())
+start_new_thread(send_data, ())
+  
+while True: 
+    pass
